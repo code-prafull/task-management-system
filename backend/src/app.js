42e311fe3376f23b4340d2efa306e18f0935348c
code-prafull <prafull.singh.cs@gmail.com>
2026-07-06 00:01:18 +0530

    Update CORS origin for task management system

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: `https://task-management-system-q9hp5t4jt-prafull-singhs-projects.vercel.app/`,
+    origin: `https://task-management-system-puce-zeta.vercel.app`,
     credentials: true
   })
 );
